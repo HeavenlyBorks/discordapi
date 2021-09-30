ca75d425b99204a892599aa15de9b3bf2d9b3e7b
Christian Beltran <34324206+HeavenlyBorks@users.noreply.github.com>
2021-09-30 20:03:53 +0000

    got events working

--- discord/bot.py
+++ discord/bot.py
@@ -1,13 +1,16 @@
 import asyncio
 import discord
+import sys
+import json
 
 class Bot():
-    def __init__(self, bot):
+    def __init__(self):
         pass
     
     # event maker
     def event(self, coro):
-        setattr(coro, coro.__name__[2:], coro)
+        setattr(self, coro.__name__[3:].upper(), coro)
+        return coro
 
     def start(self):
         # Creating client object
@@ -25,66 +28,64 @@ class Bot():
     
     # event listener
     async def listen(self, connection):  # sourcery no-metrics
-		# TODO: Rewrite pretty much this whole thing :p
-		# TODO: I have to make a requests function for sure, I could split up the listeners into a separate function.
-		while True:
-			try:
-				message = await connection.recv()
-				message = json.loads(message)
-				pretty_message = json.dumps(message, indent=2)
-				op = message["op"]
-				print('Received message from server: \n' + str(pretty_message))
-				if op == 0:
-					self.last_s = message["s"]
-					title = message["t"]
+        # TODO: Rewrite pretty much this whole thing :p
+        # TODO: I have to make a requests function for sure, I could split up the listeners into a separate function.
+        while True:
+            try:
+                message = await connection.recv()
+                message = json.loads(message)
+                pretty_message = json.dumps(message, indent=2)
+                op = message["op"]
+                print('Received message from server: \n' + str(pretty_message))
+                if op == 0:
+                    self.last_s = message["s"]
+                    title = str(message["t"])
                     if hasattr(self, title):
-                        eval(title + "()")
-                    else:
-                        pass
-					# on_ready
-					# if title == "READY":
-					# 	if self.json_v:
-					# 		with open("json/ready.json", "w", encoding="utf-8") as f:
-					# 			json.dump(message, f, ensure_ascii=False, indent=2)
-					# 	self.last_id = message["d"]["session_id"]
-					# 	if message["d"]["user"]["bot"]:
-					# 		self.user = discord.bot.Bot(message["d"]["user"])
-					# 	else:
-					# 		self.user = discord.user.User(message["d"]["user"])
-					# # on_message
-					# elif title == "MESSAGE_CREATE":
-					# 	if self.json_v:
-					# 		with open("json/message_create.json", "w", encoding="utf-8") as f:
-					# 			json.dump(message, f, ensure_ascii=False, indent=2)
-					# # on_interaction
-					# elif title == "INTERACTION_CREATE":
-					# 	if self.json_v:
-					# 		with open("json/interaction_create.json", "w", encoding="utf-8") as f:
-					# 			json.dump(message, f, ensure_ascii=False, indent=2)
-					# 	inter = discord.interaction.Interaction(message["d"])
-					# 	url = f"https://discord.com/api/v8/interactions/{inter.id}/{inter.token}/callback"
-					# 	i_json = {
-					# 		"type": 4,
-					# 		"data": {
-					# 			"content": "High Five!"
-					# 		}
-					# 	}
-					# 	r = requests.post(url, json=i_json)
-					# # on_guild_create
-					# elif title == "GUILD_CREATE":
-					# 	if self.json_v:
-					# 		with open("json/guild_create.json", "w", encoding="utf-8") as f:
-					# 			json.dump(message, f, ensure_ascii=False, indent=2)
-					# 	guild = discord.guild.Guild(message["d"])
-					# 	self.guilds.append(guild)
-				elif op == 1:
-					await self.quick_heartbeat(connection)
-				elif op == 9:
-					await self.send(connection, json.dumps(self.identify_json))
-				elif op == 10:
-					self.interval = message["d"]["heartbeat_interval"]
-			except Exception as e:
-				print(e)
-				print("- listen")
-				break
+                        await getattr(self, title)(message["d"])
+                    # on_ready
+                    # if title == "READY":
+                    #     if self.json_v:
+                    #         with open("json/ready.json", "w", encoding="utf-8") as f:
+                    #             json.dump(message, f, ensure_ascii=False, indent=2)
+                    #     self.last_id = message["d"]["session_id"]
+                    #     if message["d"]["user"]["bot"]:
+                    #         self.user = discord.bot.Bot(message["d"]["user"])
+                    #     else:
+                    #         self.user = discord.user.User(message["d"]["user"])
+                    # # on_message
+                    # elif title == "MESSAGE_CREATE":
+                    #     if self.json_v:
+                    #         with open("json/message_create.json", "w", encoding="utf-8") as f:
+                    #             json.dump(message, f, ensure_ascii=False, indent=2)
+                    # # on_interaction
+                    # elif title == "INTERACTION_CREATE":
+                    #     if self.json_v:
+                    #         with open("json/interaction_create.json", "w", encoding="utf-8") as f:
+                    #             json.dump(message, f, ensure_ascii=False, indent=2)
+                    #     inter = discord.interaction.Interaction(message["d"])
+                    #     url = f"https://discord.com/api/v8/interactions/{inter.id}/{inter.token}/callback"
+                    #     i_json = {
+                    #         "type": 4,
+                    #         "data": {
+                    #             "content": "High Five!"
+                    #         }
+                    #     }
+                    #     r = requests.post(url, json=i_json)
+                    # # on_guild_create
+                    # elif title == "GUILD_CREATE":
+                    #     if self.json_v:
+                    #         with open("json/guild_create.json", "w", encoding="utf-8") as f:
+                    #             json.dump(message, f, ensure_ascii=False, indent=2)
+                    #     guild = discord.guild.Guild(message["d"])
+                    #     self.guilds.append(guild)
+                elif op == 1:
+                    await self.quick_heartbeat(connection)
+                elif op == 9:
+                    await self.send(connection, json.dumps(self.identify_json))
+                elif op == 10:
+                    self.interval = message["d"]["heartbeat_interval"]
+            except Exception as e:
+                print(e)
+                print("- listen")
+                break
 
--- main.py
+++ main.py
@@ -1,3 +1,30 @@
-import discord
+from discord import bot
+import requests
+import os
 
-print("hi")
+urlBase = "https://discord.com/api/v8"
+bot = bot.Bot()
+
+token = os.environ['DISCORD_TOKEN']
+
+
+@bot.event
+async def on_message_create(data):
+    payload = {
+        "content": "Hello, World!",
+        "tts": False,
+        }
+    headers = {
+        "Authorization": f"Bot {token}"
+    }
+    # r = await requests.get(urlBase + "/channels/887377482741862404")
+    print("it worky??")
+    try:
+        if data["author"]["bot"]:
+            return
+    except:
+        pass
+    r = requests.post(urlBase + "/channels/887377482741862404/messages", json=payload, headers=headers)
+    print(r)
+
+bot.start()
