7e1fc93f8564b9158aa4dda7b3e10e8ed295f3f1
Christian Beltran <34324206+HeavenlyBorks@users.noreply.github.com>
2021-10-02 03:38:20 +0000

    remade channel class

--- discord/channel.py
+++ discord/channel.py
@@ -1,11 +1,28 @@
 import discord
 import json
-from munch import Munch, munchify
 
-class Channel(Munch):
-  def __init__(self, channel_obj):
-    # Turns this class into a munch.
-    super(Channel, self).__init__(channel_obj)
+__all__ = (
+    "TextChannel",
+)
+
+class TextChannel():
+  def __init__(self, c):
+    self.id = c["id"]
+    self.guild_id = c["guild_id"] or None
+    self.type = c["type"]
+    self.postition = c["position"] or None
+    self.permission_overwrites = c["permission_overwrites"] or None
+    self.name = c["name"]
+    self.topic = c["topic"] or None
+    self.nsfw = c["nsfw"] or False
+    self.last_message_id = c["last_message_id"] or None
+    self.rate_limit = c["rate_limit_per_user"] or 0
+    self.parent_id = c["parent_id"] or None
+    self.last_pin_timestamp = c["last_pin_timestamp"] or None
+
+  def send(self, message):
+      # TODO: make a message class
+      pass
   
   def __repr__(self):
     return json.dumps(self)
